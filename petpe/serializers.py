231b8da70d00204ac16d0fe851fde6814e4b874a
kichan <kimkichan1994@naver.com>
2022-08-19 19:09:12 +0900

    OroPet

--- petpe/serializers.py
+++ petpe/serializers.py
@@ -56,7 +56,7 @@ class StorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Story
-        fields = ['id', 'title', 'content', 'pictures', 'createdAt', 'updatedAt', 'user_id', 'username', 'useremail', 'userimage', 'comments', 'image_likes']
+        fields = ['id', 'content', 'pictures', 'createdAt', 'updatedAt', 'user_id', 'username', 'useremail', 'userimage', 'comments', 'image_likes']
         read_only_fields = ['image_likes']
     
     def create(self, validated_data):
